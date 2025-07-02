715951a21f2073e7fcf039e758edce38d74920a6
medhidev <76881486+medhidev@users.noreply.github.com>
2025-07-02 17:30:39 +0200

    config matrice

--- config.py
+++ config.py
@@ -1,15 +1,34 @@
+import pygame
+
 # ------- Configuration globale ----------
 
+# Matrice
 size = 500 # pixels
+cell = 15 # pixels
+matrix = [[0 for _ in range(size)] for _ in range(size)] # init
 
+# Coordonnées (x, y)
+start = (0, 0)
+end = (size-1, size-1)
 
-"""
-Algorithme traités : 
+# -------- Algorithme traités -----------
 
-- DFS
-- BFS
-- Dijkstra
-- A* (Blinky)
-- Bidirectional Search
+def draw_matrix(screen):
+    color = 'gray'
+    for i in range(size):
+        for j in range(i):
+            match((i, j)):
+                case 0 :
+                    color = '#eb4034'
+                    break
+                case 1:
+                    color = 'lime'
+                    break
 
-"""
+        pygame.draw.rect(screen, color, (i, j, cell, cell))
+
+# - DFS
+# - BFS
+# - Dijkstra
+# - A* (Blinky)
+# - Bidirectional Search
--- main.py
+++ main.py
@@ -1,46 +1,78 @@
+# Désactiver le message en console
 from os import environ
 environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 
 # Example file showing a circle moving on screen
 import pygame
 import config
+import random
 
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((config.size, config.size))
 clock = pygame.time.Clock()
 running = True
-dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+center = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+for _ in range (20):
+    print([])
+    config.matrix[random.randint(0, config.size-1)][random.randint(0, config.size-1)] = 1
 
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+    clock.tick(60)
+
+    # Events Listener
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     screen.fill("#404040")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    # config.draw_matrix(screen)
+    color = 'gray'
+    for i in range(config.size):
+        for j in range(config.size):
+            if (config.matrix[i][j] == 0):
+                color = '#eb4034'
+            elif (config.matrix[i][j] == 1):
+                color = '#1da859'
+            pygame.draw.rect(screen, color, (i, j, config.cell, config.cell))
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+    # pygame.draw.circle(screen, "red", player_pos, 40)
+    
 
-    # flip() the display to put your work on screen
+
+    # keys = pygame.key.get_pressed()
+    # if keys[pygame.K_w]:
+    #     player_pos.y -= 300 * dt
+    # if keys[pygame.K_s]:
+    #     player_pos.y += 300 * dt
+    # if keys[pygame.K_a]:
+    #     player_pos.x -= 300 * dt
+    # if keys[pygame.K_d]:
+    #     player_pos.x += 300 * dt
+
+
+
+    # Display
     pygame.display.flip()
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
+# def color_matrix():
+#     for i in range(config.size):
+#         for j in range(i):
+#             match((i, j)):
+#                 # Départ - Mur - Arrivée
+#                 case 0 :
+#                     pygame.draw.rect(screen, '#eb4034' , (50, 50, 100, 100))
+#                     break
+#                 case 1:
+#                     print('mur')
+#                     break
+#                 case -1:
+#                     print('fin')
+#                     break
+
+#             # Couleurs cases Algorithme
 
 pygame.quit()
